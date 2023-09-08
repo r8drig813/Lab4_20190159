e8aa4ec7b0c3d49e74cd65dda014cb6c026300e7
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2023-09-07 20:42:15 -0500

    Primer Commit

---
+++ lab4_20190159/src/main/java/com/example/lab4_20190159/Lab420190159Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20190159;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420190159Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420190159Application.class, args);
+    }
+
+}
---
+++ lab4_20190159/src/test/java/com/example/lab4_20190159/Lab420190159ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20190159;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420190159ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
