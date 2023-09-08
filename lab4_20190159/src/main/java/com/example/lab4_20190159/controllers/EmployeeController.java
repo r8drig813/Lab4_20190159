7e82b0ee92b1d86d0036b12ff02e3c5558b84443
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2023-09-07 21:56:06 -0500

    Edit

--- lab4_20190159/src/main/java/com/example/lab4_20190159/controllers/EmployeeController.java
+++ lab4_20190159/src/main/java/com/example/lab4_20190159/controllers/EmployeeController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -38,5 +40,27 @@ public class EmployeeController {
         model.addAttribute("textoBuscado", searchField);
 
         return "employee/lista";
+    }
+
+
+    @GetMapping("/edit")
+    public String editar(Model model, @RequestParam("id") int id) {
+
+        Optional<Employees> optEmployee = employeeRepository.findById(id);
+
+        if (optEmployee.isPresent()) {
+            Employees shipper = optEmployee.get();
+            model.addAttribute("employee", shipper);
+            return "employee/editFrm";
+        } else {
+            return "redirect:/employee/list";
+        }
+    }
+
+    @PostMapping("/update")
+    public String updateOnlyPhone(Employees shipper) {
+        //employeeRepository.actualizarTelefono(shipper.getPhone(),shipper.getShipperId());
+
+        return "redirect:/employee/list";
     }
 }
--- lab4_20190159/src/main/java/com/example/lab4_20190159/repository/EmployeeRepository.java
+++ lab4_20190159/src/main/java/com/example/lab4_20190159/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.example.lab4_20190159.repository;
 
 import com.example.lab4_20190159.entity.Employees;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,11 @@ public interface EmployeeRepository extends JpaRepository<Employees, Integer> {
 
     @Query(nativeQuery = true, value = "SELECT * FROM employees where first_name = ?1 or last_name = ?1 ")
     List<Employees> buscarPorNombre(String textoIngreso);
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE employee SET phone_number = ?1 and salary = ?2 WHERE employee = ?3")
+    void actualizar(String phone_number, double salary, int employee_id);
 
 }
